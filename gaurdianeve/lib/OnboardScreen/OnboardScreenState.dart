9c6d7b8b7aec54f7aeaf1cabe35a3a5105a52140
gaurdianEve <gaurdianeve@gmail.com>
2024-01-26 18:45:19 +0530

    Update OnboardScreenState.dart

--- gaurdianeve/lib/OnboardScreen/OnboardScreenState.dart
+++ gaurdianeve/lib/OnboardScreen/OnboardScreenState.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -36,15 +37,14 @@ class _OnboardScreenState extends State<OnboardScreen> {
       body: Stack(
         children: [
           AnimatedPositioned(
-            top: isAuthenticationDialogShow?-50:0,
+            top: isAuthenticationDialogShow ? -height : 0,
             height: height,
             width: width,
-            duration: Duration(milliseconds: 240),
+            duration: Duration(milliseconds: 420),
             child: PageView.builder(
               controller: _pageController,
               itemCount: _onboardingPages.length,
               onPageChanged: (int page) {
-               
                 setState(() {
                   _currentPage = page;
                 });
@@ -59,20 +59,27 @@ class _OnboardScreenState extends State<OnboardScreen> {
             bottom: height / 7,
             child: GestureDetector(
               onTap: () {
-                if (_currentPage==_onboardingPages.length-1 ) {
+                print(height);
+                if (_currentPage == _onboardingPages.length - 1) {
                   setState(() {
-                    isAuthenticationDialogShow!= isAuthenticationDialogShow;
+                    isAuthenticationDialogShow = true;
                   });
+                  Future.delayed(
+                    Duration(milliseconds: 800),
+                    (){
+                      customDialogBox(context,onChange:(value) => setState(() {
+                        isAuthenticationDialogShow = false;
+                      }),);
+                    }
                     
-                  }
-                  else{
-                     _pageController.nextPage(
-                  duration: const  Duration(milliseconds: 500),
-                  curve: Curves.easeInCubic,
-                );
-                    
-                  }
-               
+                  );
+                  
+                } else {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInCubic,
+                  );
+                }
               },
               child: const CircleAvatar(
                   radius: 32,
@@ -85,39 +92,65 @@ class _OnboardScreenState extends State<OnboardScreen> {
           ),
           AnimatedPositioned(
             duration: Duration(milliseconds: 240),
-            right: width/12,
-            top: isAuthenticationDialogShow?-50:height/12,
+            right: width / 12,
+            top: isAuthenticationDialogShow ? -50 : height / 12,
             child: TextButton(
                 style: ButtonStyle(
-                  padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(6)),
-                  backgroundColor: MaterialStateProperty.all(grey),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50.0),
-              
-            )
-                  )
-                ),
-                onPressed: (){
-                  
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.all(6)),
+                    backgroundColor: MaterialStateProperty.all(grey),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ))),
+                onPressed: () {
                   setState(() {
-                   
-                    _pageController.jumpTo(width*(_onboardingPages.length-_currentPage));
-                    
+                    _pageController.jumpTo(
+                        width * (_onboardingPages.length - _currentPage));
                   });
-                  
-
                 },
-                child:  Text(
-                  "Skip",
-                  style: GoogleFonts.poppins(color: Colors.white,fontWeight:FontWeight.w300,letterSpacing: 1,fontSize: 16.sp)
-                )
-              ),
+                child: Text("Skip",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        letterSpacing: 1,
+                        fontSize: 16.sp))),
           ),
         ],
       ),
       backgroundColor: scaffoldBackgroundColor,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    
     );
+  }
+
+  Future<Object?> customDialogBox(BuildContext context,{required ValueChanged onChange}) {
+      double width = MediaQuery.of(context).size.width;
+      double height = MediaQuery.of(context).size.height;
+    print(width);
+    return showGeneralDialog(
+                  context: context,
+                  barrierDismissible: true,
+                  barrierLabel: "Sign In",
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return Center(
+                      child: Stack(
+                        children: [
+                          Container(
+                        
+                        height:height/1.832 ,
+                        width:width/1.256,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+
+                        ),
+                        
+
+                      )
+                        ],
+                      ),
+                    );
+                  },
+                ).then(onChange);
   }
 }
