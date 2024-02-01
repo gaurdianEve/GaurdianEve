a53a223a0c5462417ccfb1ddc3c1bcb08e41d22a
gaurdianEve <gaurdianeve@gmail.com>
2024-02-01 20:25:37 +0530

    login

--- gaurdianeve/lib/Authentication/bloc/auth_b_loc_bloc.dart
+++ gaurdianeve/lib/Authentication/bloc/auth_b_loc_bloc.dart
@@ -1,13 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:meta/meta.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
 
 part 'auth_b_loc_event.dart';
 part 'auth_b_loc_state.dart';
 
+
 class AuthBLocBloc extends Bloc<AuthBLocEvent, AuthBLocState> {
   AuthBLocBloc() : super(AuthBLocInitial()) {
-    on<AuthBLocEvent>((event, emit) {
-      // TODO: implement event handler
+    on<LoginEvent>((event, emit) {
+      final firebase = FirebaseAuth.instance;
+      firebase.signInWithEmailAndPassword(email: "sarvarsheikh891@gmail.com", password: "");
     });
   }
 }
